b648aaad12b97c4257e776034ffbb62fafd5ac63
judiciousmurich <murichjudicious45@gmail.com>
2025-01-02 13:05:25 +0300

    feat:add data

--- scripts/data_preprocessing/clean_data.py
+++ scripts/data_preprocessing/clean_data.py
@@ -7,9 +7,8 @@ data = pd.read_csv('../data/raw/kevin_cookie_sales.csv')
 data = data.drop_duplicates()
 data = data.fillna(0) 
 
-# Example: Convert date column to datetime
+
 data['Sale_Date'] = pd.to_datetime(data['Sale_Date'])
 
-# Save cleaned data to a new file
 data.to_csv('../data/transformed/kevin_cookie_sales_cleaned.csv', index=False)
 print("Data cleaned and saved.")
